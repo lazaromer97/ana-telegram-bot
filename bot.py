3ae0a29dd92c0f494154fcb2aa014637071eae40
Lazaro Estrada <lazaromer97@gmail.com>
2021-01-31 02:58:29 -0500

    Create bot structure

---
+++ bot.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+from telegram.ext import Updater, CommandHandler
+
+def ayuda(update, context):
+    update.message.reply_text("La ayuda estara disponible aqui")
+
+if __name__ == '__main__':
+    load_dotenv()
+
+    TOKEN = os.getenv('TOKEN')
+
+    updater = Updater(token=TOKEN, use_context=True)
+
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('ayuda', ayuda))
+
+    updater.start_polling()
+    updater.idle()
